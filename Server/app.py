05e3a2c8f29256a06ead9cad043394ef944c267b
Anukalp Agarwal <anukalpagarwal24@gmail.com>
2026-09-04 22:11:23 +0530

    fix: update backend imports and production CORS

--- Server/app.py
+++ Server/app.py
@@ -2,11 +2,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 app  = FastAPI()
-from Server.routes.Ask import router as ask_router
-from Server.routes.Upload import router as upload_router
+from routes.Ask import router as ask_router
+from routes.Upload import router as upload_router
 app.add_middleware(
     CORSMiddleware ,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:8000"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:8000" , "https://askly-mo5d.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
